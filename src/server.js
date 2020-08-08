1d479d706dc80a1305235198b217eea5f561a7ec
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-08 12:31:04 -0300

    study

--- src/server.js
+++ src/server.js
@@ -22,19 +22,47 @@ const proffys = [
         time_to: [1800]
     }
 ];
-function pageLanding(res,res) {
-    return res.sendFile(__dirname + '/views/index.html');
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+function pageLanding(req,res) {
+    return res.render("index.html");
 }
-function pageStudy(res,res) {
-    return res.sendFile(__dirname + '/views/study.html');
+function pageStudy(req,res) {
+    const filters = req.query;
+    return res.render("study.html", { proffys, filters, subjects, weekdays });
 }
-function pageGiveClasses(res,res) {
-    return res.sendFile(__dirname + '/views/give-classes.html');
+function pageGiveClasses(req,res) {
+    return res.render("give-classes.html");
 }
 const express = require('express');
 const server = express();
-server.use(express.static('public'));
-server.get('/', pageLanding);
-server.get('/study', pageStudy)
-server.get('/give-classes', pageGiveClasses)
-server.listen(5500);
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+server
+.use(express.static('public'))
+.get('/', pageLanding)
+.get('/study', pageStudy)
+.get('/give-classes', pageGiveClasses)
+.listen(5500);
