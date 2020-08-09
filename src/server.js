e4549f06d4f60b11b0eb8391c66d8d06cfd13a7b
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-08 22:29:43 -0300

    db

--- src/server.js
+++ src/server.js
@@ -12,5 +12,5 @@ server
 .get('/', pageLanding)
 .get('/study', pageStudy)
 .get('/give-classes', pageGiveClasses)
-.post('/save-class', saveClasses)
+.post('/save-classes', saveClasses)
 .listen(5500);
