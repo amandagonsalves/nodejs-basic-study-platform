60ef6659eebc77e3292da79317f5354329bc8c13
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-11 11:33:55 -0300

    button field

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -2,9 +2,13 @@ document.querySelector('#add-time').addEventListener('click', cloneField);
 function cloneField(e) {
     e.preventDefault();
     const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
-    const fields = newFieldContainer.querySelectorAll('input')
+    const fields = newFieldContainer.querySelectorAll('input');
     fields.forEach(field => {
         field.value = '';
     })
+    const button = newFieldContainer.querySelector('.buttonField');
+    console.log(button)
+    button.style.display = 'block';
+    newFieldContainer.classList.add('newFieldContainer')
     document.querySelector('#schedule-items').appendChild(newFieldContainer);
 }
